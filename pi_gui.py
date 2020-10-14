8b3efbbfcf7f8c22f95517ef74a7f25311d7d018
Karl Mueller <muellerkarlw@gmail.com>
2020-10-13 22:49:57 -0400

    Add the bno_simpletest method
    Adding the stock code from bno_simpletest for modification for project needs. Modified versions of this code are for a feature/imu_out branch only for imu output

---
+++ pi_gui.py
@@ -0,0 +1,11 @@
+import Tkinter
+import tkMessageBox
+from interrupt.py import interrupt
+
+top = Tkinter.Tk()
+
+cncl = Tkinter.Button(top, text = "Click to CTRL+C", command = )
+
+
+cncl.pack()
+top.mainloop()
